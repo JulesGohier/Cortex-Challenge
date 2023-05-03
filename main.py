9d58a1c3d4b671a1b781c17383b798f1a2bb826b
Jules Gohier <jules.reihog5@gmail.com>
2023-05-03 10:43:56 +0200

    The creation of an algorithm to solve the challenge of the cortex calculations. And the addition of comments

--- main.py
+++ main.py
@@ -3,20 +3,23 @@ from script import basic_fonction
 from script import algo_duplication
 from script import algo_frequency
 from script import algo_missing
+from script import algo_calculation
 
 if __name__ == '__main__':
     game_type: str = ""
     game_data1 = None
     game_data2 = None
-    card: dict = basic_fonction.open_json_card("card-5") #14
+    card: dict = basic_fonction.open_json_card("card-3")  # 14
 
     game_type, game_data1, game_data2 = basic_fonction.recovery_data_card(card, game_type, game_data1, game_data2)
 
     if game_type == "couleur":
         algo_color.algo_color(game_data1)
     if game_type == "doublon":
-        basic_fonction.calculate_time_execution(algo_duplication.algo_duplication, game_data1, 10000)
+        algo_duplication.algo_duplication(game_data1)
     if game_type == "fréquence":
         algo_frequency.algo_frequency(game_data1)
     if game_type == "manquant":
         algo_missing.algo_missing(game_data1)
+    if game_type == "calcul":
+        algo_calculation.algo_calculation(game_data1, game_data2)
---
+++ script/algo_calculation.py
@@ -0,0 +1,24 @@
+def algo_calculation(number_find: int, number_list: list):
+    """
+    A function that allows finding the addition sequence for the "number_find" bet the numbers of the sequence
+    "number_list" (inspired by the algortihme on backpack)
+    :param number_find: Number to search
+    :param number_list: List of start numbers
+    :return: A string of characters where the numbers appear in ascending order and are separated by the + sign.
+    """
+    temporary_max_value = []
+    temporary_combination = []
+
+    for i in range(number_find + 1):
+        temporary_max_value.append(0)
+        temporary_combination.append([])
+
+    for item in number_list:
+        for w in range(number_find, item - 1, -1):
+            if item + temporary_max_value[w - item] > temporary_max_value[w]:
+                temporary_max_value[w] = item + temporary_max_value[w - item]
+                temporary_combination[w] = temporary_combination[w - item] + [item]
+
+    final_combination = temporary_combination[number_find]
+    final_combination.sort()
+    return '+'.join(str(elem) for elem in final_combination)
