6574c0dc6f78750ac49c9217850209b89414179c
Calliste <c.meslard@hotmail.com>
2023-05-15 02:23:08 +0200

    📝 Add comments
    Add comments for algo_reflection and algo_reasoning.

--- script/algo_reasoning.py
+++ script/algo_reasoning.py
@@ -1,4 +1,11 @@
 def y_start(matrix, rows, cols):
+    """
+    Find the starting row index of the '' characters in the matrix
+    :param matrix: drawing
+    :param rows: numbers of rows in the matrix
+    :param cols: numbers of cols in the matrix
+    :return: y start
+    """
     for i in range(rows):
         for j in range(cols):
             if matrix[i][j] != 'X':
@@ -6,24 +13,41 @@ def y_start(matrix, rows, cols):
 
 
 def x_start(matrix, rows, cols):
+    """
+    Find the starting column index of the '' characters in the matrix
+    :param matrix: drawing
+    :param rows: numbers of rows in the matrix
+    :param cols: numbers of cols in the matrix
+    :return: x start
+    """
     for j in range(cols):
         for i in range(rows):
             if matrix[i][j] != 'X':
                 return i
 
 
-def y_end(matrix):
-    rows = len(matrix)
-    cols = len(matrix[0])
+def y_end(matrix, rows, cols):
+    """
+    Find the ending row index of the '' characters in the matrix
+    :param matrix: drawing
+    :param rows: numbers of rows in the matrix
+    :param cols: numbers of cols in the matrix
+    :return: y end
+    """
     for i in range(rows-1, -1, -1):
         for j in range(cols-1, -1, -1):
             if matrix[i][j] != 'X':
                 return j
 
 
-def x_end(matrix):
-    rows = len(matrix)
-    cols = len(matrix[0])
+def x_end(matrix, rows, cols):
+    """
+    Find the ending column index of the '' characters in the matrix
+    :param matrix: drawing
+    :param rows: numbers of rows in the matrix
+    :param cols: numbers of cols in the matrix
+    :return: x end
+    """
     for j in range(cols-1, -1, -1):
         for i in range(rows-1, -1, -1):
             if matrix[i][j] != 'X':
@@ -31,6 +55,13 @@ def x_end(matrix):
 
 
 def compare_size(matrix2, length_matrix1, width_matrix1):
+    """
+    Compare the size of two matrices and return True if they are the same size, False otherwise
+    :param matrix2: a piece
+    :param length_matrix1: piece length acceptable by the drawing
+    :param width_matrix1: piece length acceptable by the drawing
+    :return: boolean
+    """
     length_matrix2 = len(matrix2)
     width_matrix2 = len(matrix2[0])
     if length_matrix1 == length_matrix2 and width_matrix1 == width_matrix2:
@@ -40,6 +71,14 @@ def compare_size(matrix2, length_matrix1, width_matrix1):
 
 
 def fits_in(matrix1, matrix2, x0, y0):
+    """
+    Verify if the piece enter exactly in the drawing
+    :param matrix1: drawing
+    :param matrix2: piece
+    :param x0: locate where the piece inserts in the drawing
+    :param y0: locate where the piece inserts in the drawing
+    :return: boolean
+    """
     rows2 = len(matrix2)
     cols2 = len(matrix2[0])
     for i in range(rows2):
@@ -50,15 +89,22 @@ def fits_in(matrix1, matrix2, x0, y0):
 
 
 def algo_reasoning(matrix: list, piece: dict):
+    """
+    Given a matrix and a dictionary of pieces, determines which piece can fit into the matrix.
+    :param matrix: drawing
+    :param piece: dict with the pieces
+    :return: piece which fits in the drawing
+    """
+    # Find the starting and ending row and column indices of the '' characters in the matrix
     rows = len(matrix)
     cols = len(matrix[0])
     x0 = x_start(matrix, rows, cols)
     y0 = y_start(matrix, rows, cols)
-    length_matrix = x_end(matrix) - x0 + 1
-    width_matrix = y_end(matrix) - y0 + 1
+    length_matrix = x_end(matrix, rows, cols) - x0 + 1  # enlever les variables x0 et y0
+    width_matrix = y_end(matrix, rows, cols) - y0 + 1
 
-    #lenght_piece = len(piece["A"])
-    #width_piece = len(piece["A"][0])
+    # length_piece = len(piece["A"])
+    # width_piece = len(piece["A"][0])
 
     if compare_size(piece["A"], length_matrix, width_matrix):
         if fits_in(matrix, piece["A"], x0, y0):
--- script/algo_reflection.py
+++ script/algo_reflection.py
@@ -1,79 +1,98 @@
-def normal_turn(y, x, direction):
+def normal_reflect(y, x, light_direction):
+    """
+    Reflects the light beam when encountering a '/' mirror : change its direction and move by one cell
+    :param y: y-coordinate of the current '/' cell
+    :param x: x-coordinate of the current '/' cell
+    :param light_direction: current light direction
+    :return: new light direction
+    """
 
-    if direction == "North":
-        direction = "East"
+    if light_direction == "North":
+        light_direction = "East"
         x = x + 1
-        return y, x, direction
+        return y, x, light_direction
 
-    if direction == "South":
-        direction = "West"
+    if light_direction == "South":
+        light_direction = "West"
         x = x - 1
-        return y, x, direction
+        return y, x, light_direction
 
-    if direction == "West":
-        direction = "South"
+    if light_direction == "West":
+        light_direction = "South"
         y = y + 1
-        return y, x, direction
+        return y, x, light_direction
 
-    if direction == "East":
-        direction = "North"
+    if light_direction == "East":
+        light_direction = "North"
         y = y - 1
-        return y, x, direction
+        return y, x, light_direction
 
 
-def inverted_turn(y, x, direction):
+def inverted_reflect(y, x, light_direction):
+    """
+    Reflects the light beam when encountering a '\\' mirror : change its direction and move by one cell
+    :param y: y-coordinate of the current '\\' cell
+    :param x: x-coordinate of the current '\\' cell
+    :param light_direction: current light direction
+    :return: new light direction
+    """
 
-    if direction == "North":
-        direction = "West"
+    if light_direction == "North":
+        light_direction = "West"
         x = x - 1
-        return y, x, direction
+        return y, x, light_direction
 
-    if direction == "South":
-        direction = "East"
+    if light_direction == "South":
+        light_direction = "East"
         x = x + 1
-        return y, x, direction
+        return y, x, light_direction
 
-    if direction == "West":
-        direction = "North"
+    if light_direction == "West":
+        light_direction = "North"
         y = y - 1
-        return y, x, direction
+        return y, x, light_direction
 
-    if direction == "East":
-        direction = "South"
+    if light_direction == "East":
+        light_direction = "South"
         y = y + 1
-        return y, x, direction
+        return y, x, light_direction
 
 
 def algo_reflection(matrix: list):
+    """
+    Simulate the travel of a light beam to solve the reflection problem
+    :param matrix: map with a lamp (L)(=light beam start), numbers (1-15) and mirrors (/ and \\)
+    :return: number reached by the light beam
+    """
     y, x = 5, 2
     cell = matrix[y][x]
-    direction = "North"
+    light_direction = "North"
     result = ''
     while result == '':
         while cell in ('', "L"):
 
-            if direction == "North":
+            if light_direction == "North":
                 y = y - 1
                 cell = matrix[y][x]
 
-            elif direction == "South":
+            elif light_direction == "South":
                 y = y + 1
                 cell = matrix[y][x]
 
-            elif direction == "West":
+            elif light_direction == "West":
                 x = x - 1
                 cell = matrix[y][x]
 
-            elif direction == "East":
+            elif light_direction == "East":
                 x = x + 1
                 cell = matrix[y][x]
 
         if cell == "/":
-            y, x, direction = normal_turn(y, x, direction)
+            y, x, light_direction = normal_reflect(y, x, light_direction)
             cell = matrix[y][x]
 
         elif cell == "\\":
-            y, x, direction = inverted_turn(y, x, direction)
+            y, x, light_direction = inverted_reflect(y, x, light_direction)
             cell = matrix[y][x]
 
         else:
